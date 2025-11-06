f9c71c3e8192a9e750fa645d0a7a74861dc58868
xiaodaxia <xiaozisheng2008@hotmail.com>
2025-11-06 18:56:47 +0800

    fix cpp snippets and add copy file path user commands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -20,3 +20,46 @@
 --     vim.wo.conceallevel = 0
 --   end,
 -- })
+
+local api = vim.api
+local fn = vim.fn
+
+-- Helper to copy text to system clipboard
+local function copy_to_clipboard(text)
+  if fn.has("mac") == 1 then
+    fn.setreg("+", text)
+  else
+    fn.setreg("+", text)
+  end
+  vim.notify("Copied to clipboard: " .. text, vim.log.levels.INFO)
+end
+
+-- Copy file name (basename)
+api.nvim_create_user_command("CopyFileName", function()
+  local fname = fn.expand("%:t")
+  if fname == "" then
+    vim.notify("No file name", vim.log.levels.WARN)
+    return
+  end
+  copy_to_clipboard(fname)
+end, { nargs = 0 })
+
+-- Copy full file path
+api.nvim_create_user_command("CopyFilePath", function()
+  local fpath = fn.expand("%:p")
+  if fpath == "" then
+    vim.notify("No file path", vim.log.levels.WARN)
+    return
+  end
+  copy_to_clipboard(fpath)
+end, { nargs = 0 })
+
+-- Copy file parent directory
+api.nvim_create_user_command("CopyFileDirectory", function()
+  local fpath = fn.expand("%:p:h")
+  if fpath == "" then
+    vim.notify("No file path", vim.log.levels.WARN)
+    return
+  end
+  copy_to_clipboard(fpath)
+end, { nargs = 0 })
